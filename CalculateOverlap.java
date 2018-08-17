5cc6d2eb50baf9c2783f142614e8f876409d1595
szymslow12 <szym.slow12@gmail.com>
2018-08-17 22:51:59 +0200

    add new constructor with 8 ints as parameters, fix getOverlap boolean

--- CalculateOverlap.java
+++ CalculateOverlap.java
@@ -3,6 +3,14 @@ public class CalculateOverlap {
     private Rectangle r2;
     private int overlap;
 
+    public CalculateOverlap(int bottomX, int bottomY, int topX, int topY,
+                            int bottomX2, int bottomY2, int topX2, int topY2) {
+
+        this.r1 = new Rectangle(bottomX, bottomY, topX, topY);
+        this.r2 = new Rectangle(bottomX2, bottomY2, topX2, topY2);
+        this.overlap = getOverlap();
+    }
+
     public CalculateOverlap(Rectangle r1, Rectangle r2) {
         this.r1 = r1;
         this.r2 = r2;
@@ -22,13 +30,16 @@ public class CalculateOverlap {
                     r2.getBottomLeftX() <= r1.getTopRightX() &&
                     r2.getBottomLeftY() <= r1.getTopRightY()) {
 
-            if (r2.getBottomLeftX() <= r1.getBottomLeftX() && r2.getBottomLeftY() >= r1.getBottomLeftY() &&
-                    r2.getTopRightX() <= r1.getTopRightX() && r2.getTopRightY() <= r1.getTopRightY()) {
+            if ((r2.getBottomLeftX() <= r1.getBottomLeftX() || r2.getBottomLeftX() >= r1.getBottomLeftX()) &&
+                r2.getBottomLeftY() >= r1.getBottomLeftY() &&
+                r2.getTopRightX() <= r1.getTopRightX() &&
+                r2.getTopRightY() <= r1.getTopRightY()) {
+
                 return r2.calculateArea();
             } else {
                 return r1.calculateArea();
             }
-            
+
         } else if (r1.getBottomLeftX() <= r2.getTopRightX() &&
                     r1.getBottomLeftY() <= r2.getTopRightY()){
 
@@ -48,13 +59,5 @@ public class CalculateOverlap {
 
     public int overlap() {
         return this.overlap;
-    }
-
-    public static void main(String[] args) {
-        Rectangle r1 = new Rectangle(-1, -1, 10, 10);
-        Rectangle r2 = new Rectangle(-1, 0, 3, 9);
-        CalculateOverlap calculateOverlap = new CalculateOverlap(r1, r2);
-        System.out.println(calculateOverlap.overlap());
-
     }
 }
